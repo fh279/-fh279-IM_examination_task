c3894172f314a9836a8fbd930a73df2cff7eaad2
fh279 <fh279@yandex.ru>
2022-05-18 18:35:12 +0300

    and more editions..

--- src/com/mpogorelov/professions/Driver.java
+++ src/com/mpogorelov/professions/Driver.java
@@ -1,8 +1,6 @@
 package com.mpogorelov.professions;
 
 public class Driver extends Person {
-    private String fullName; // по схеме это поля в классе Driver нет, но есть по описанию.
-    // В итоге сделаю так, что поле fullName есть и оно будет наследоваться дочерним классом Person.
     private int experience;
 
     public Driver(String fullName, int age, int experience) {
@@ -10,31 +8,17 @@ public class Driver extends Person {
         this.experience = experience;
     }
 
-
-    public String getFullName() {
-        return fullName;
-    }
-
     public int getExperience() {
         return experience;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
     }
 
     public void setExperience(int experience) {
         this.experience = experience;
     }
 
-  /*  public Driver(String fullName, int experience) {
-        this.fullName = fullName;
-        this.experience = experience;
-    }*/
-
     @Override
     public String toString() {
-        return "Full name of the driver: " + fullName
+        return "Full name of the driver: " + getFullName()
                 + "years of experience: " + experience
                 + "age of the driver, years" + getAge();
     }
